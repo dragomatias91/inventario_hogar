cd89e9ac41deefbc2c16a71e050c8c8e61a085fa
dragomatias91 <dragomatias91@gmail.com>
2026-04-09 16:42:10 -0300

    modificacion models categoria

--- src/categorias/models.py
+++ src/categorias/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Categoria(models.Model):
     nombre= models.CharField(max_length=20)
-    descripcion= models.TextField()
+    descripcion= models.TextField(null=True , blank=True )
