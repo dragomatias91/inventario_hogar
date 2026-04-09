9d293f2107dd49403355d03b4232be4beae30575
dragomatias91 <dragomatias91@gmail.com>
2026-04-09 17:09:59 -0300

    modelo producto

--- src/productos/models.py
+++ src/productos/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from categorias.models import Categoria
 
-# Create your models here.
+class Producto(models.Model):
+
+    class UnidadMedida(models.TextChoices):
+        KG = 'KG', 'Kilogramo'
+        GR = 'GR', 'Gramo'
+        LT = 'LT', 'Litro'
+        ML = 'ML', 'Mililitro'
+        UNIDAD = 'UNIDAD', 'Unidad'
+
+    class Ubicacion(models.TextChoices):
+        ALACENA = 'ALACENA', 'Alacena'
+        HELADERA = 'HELADERA', 'Heladera'
+        FREEZER = 'FREEZER', 'Freezer'
+        DESPENSA = 'DESPENSA', 'Despensa'
+
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(null=True, blank=True)
+    cantidad = models.IntegerField()
+    unidad_medida = models.CharField(max_length=10, choices=UnidadMedida.choices)
+    stock_minimo = models.IntegerField()
+    fecha_vencimiento = models.DateField(null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    ubicacion = models.CharField(max_length=10, choices=Ubicacion.choices)
+    disponible = models.BooleanField(default=True)
