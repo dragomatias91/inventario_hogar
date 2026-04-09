a85711525caa7d453dc8d6537616f32ebcb4fe1a
dragomatias91 <dragomatias91@gmail.com>
2026-04-09 18:04:04 -0300

    modifico admin moderno

--- src/categorias/admin.py
+++ src/categorias/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Categoria
 
-
-admin.site.register(Categoria)
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "descripcion")
+    search_fields = ("nombre",)
--- src/productos/admin.py
+++ src/productos/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-
 from .models import Producto
 
-admin.site.register(Producto)
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "cantidad", "unidad_medida", "ubicacion", "disponible")
+    search_fields = ("nombre",)
