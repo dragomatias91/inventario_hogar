d35049d75821f35ae1a10e19b5d859b88c173e31
dragomatias91 <dragomatias91@gmail.com>
2026-04-10 20:04:10 -0300

    visualizacion productos

--- src/productos/views.py
+++ src/productos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Producto
 
-# Create your views here.
+def lista_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'productos/lista_productos.html', {'productos': productos})
+
