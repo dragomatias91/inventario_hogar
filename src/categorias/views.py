fcc26d04fede13bd8d8b9781c35ef260e9abd73d
dragomatias91 <dragomatias91@gmail.com>
2026-04-10 19:58:59 -0300

    config categoria vista

--- src/categorias/views.py
+++ src/categorias/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Categoria
 
-# Create your views here.
+def lista_categorias(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'categorias/lista_categorias.html', {'categorias': categorias})
