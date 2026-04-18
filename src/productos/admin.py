4620f6ff7596269def519ea123ee090233c2d4f4
dragomatias91 <dragomatias91@gmail.com>
2026-04-18 13:06:05 -0300

    admin lista

--- src/productos/admin.py
+++ src/productos/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto , ListaCompras
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ("nombre", "cantidad", "unidad_medida", "ubicacion", "disponible")
     search_fields = ("nombre",)
     list_filter=("disponible",)
+
+
+@admin.register(ListaCompras)
+class ListaComprasAdmin(admin.ModelAdmin):
+    list_display = ("producto", "cantidad_necesaria", "comprado")
+    list_filter = ("comprado",)
+    search_fields = ("producto__nombre",)
