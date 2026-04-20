e64c64ba4965f2383479468584a670bf96112561
dragomatias91 <dragomatias91@gmail.com>
2026-04-20 13:41:54 -0300

    Crud Categoria v2

--- src/categorias/urls.py
+++ src/categorias/urls.py
@@ -6,4 +6,6 @@ app_name = 'categorias'
 urlpatterns = [
     path('', views.lista_categorias, name='lista'),
     path('crear/', views.crear_categoria, name='crear'),
+    path('editar/<int:pk>/', views.editar_categoria, name='editar'),
+    path('eliminar/<int:pk>/', views.eliminar_categoria, name='eliminar'),
 ]
--- src/categorias/views.py
+++ src/categorias/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect , get_object_or_404 
 from .models import Categoria
 from .forms import CategoriaForm
 
@@ -15,3 +15,23 @@ def crear_categoria(request):
     else:
         form = CategoriaForm()
     return render(request, 'categorias/crear_categoria.html', {'form': form})
+
+def editar_categoria(request, pk):
+    categoria = get_object_or_404(Categoria, pk=pk)
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            form.save()
+            return redirect('categorias:lista')
+    else:
+        form = CategoriaForm(instance=categoria)
+    return render(request, 'categorias/editar_categoria.html', {'form': form})
+
+def eliminar_categoria(request, pk):
+    categoria = get_object_or_404(Categoria, pk=pk)
+    if request.method == 'POST':
+        categoria.delete()
+        return redirect('categorias:lista')
+    return render(request, 'categorias/eliminar_categoria.html', {'categoria': categoria})
+
+
