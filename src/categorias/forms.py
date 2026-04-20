fe0ef3041eb3a217a991e4fd78e61fb86ed3a406
dragomatias91 <dragomatias91@gmail.com>
2026-04-20 12:27:38 -0300

    Modificacion visual form categoria

--- src/categorias/forms.py
+++ src/categorias/forms.py
@@ -5,3 +5,7 @@ class CategoriaForm(forms.ModelForm):
     class Meta:
         model = Categoria
         fields = ['nombre', 'descripcion']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
